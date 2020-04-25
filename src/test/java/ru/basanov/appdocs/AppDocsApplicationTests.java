25b60c2ee37eff329667da1c05965d96d367186c
mingiyan1987 <basanov.mt87@gmail.com>
2020-04-25 13:28:47 +0300

    Создал приложение.

---
+++ src/test/java/ru/basanov/appdocs/AppDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.basanov.appdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
